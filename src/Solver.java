211c02897277be30de5dabe716bbc3103d4e0343
melindakuo <melindakuomk@gmail.com>
2023-07-17 13:49:09 -0700

    changed testing

--- src/Solver.java
+++ src/Solver.java
@@ -1,5 +1,4 @@
 public class Solver {
-
     public static void main(String[] args) throws Exception {
         int[][] board = {{0,0,0,0,0,0,1,0,0},
                          {0,7,0,6,0,1,0,9,0},
@@ -10,13 +9,12 @@ public class Solver {
                          {3,8,0,7,6,0,0,0,4},
                          {0,6,0,5,0,8,0,7,0},
                          {0,0,5,0,0,0,0,0,0}};
-        solve(board);
+        // solve(board);
         System.out.println(board);
-        //test
     }
 
 
-    public static boolean validator(int[][] board, int row, int col, int spot) {
+    public boolean validator(int[][] board, int row, int col, int spot) {
         int boxrow = row-row%3;
         int boxcol = col-col%3;
         for(int i = 0; i<9; i++){
@@ -35,7 +33,7 @@ public class Solver {
         return true;
     }
 
-    public static boolean solve(int[][] board){
+    public boolean solve(int[][] board){
         for(int i =0; i<board.length; i++){
             for(int j = 0; j<board[0].length; j++){
                 if(board[i][j]== 0){
