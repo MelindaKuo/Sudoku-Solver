a0c8a3a76576a1d6b8853244586212fdccc77dba
melindakuo <melindakuomk@gmail.com>
2023-07-19 22:45:53 -0700

    changed Solver

--- src/Solver.java
+++ src/Solver.java
@@ -1,20 +1,8 @@
 public class Solver {
-    public static void main(String[] args) throws Exception {
-        int[][] board = {{0,0,0,0,0,0,1,0,0},
-                         {0,7,0,6,0,1,0,9,0},
-                         {1,0,0,0,8,9,0,6,5},
-                         {0,0,0,8,2,0,9,0,7},
-                         {0,5,0,9,0,6,0,2,0},
-                         {9,0,7,0,3,4,0,0,0},
-                         {3,8,0,7,6,0,0,0,4},
-                         {0,6,0,5,0,8,0,7,0},
-                         {0,0,5,0,0,0,0,0,0}};
-        // solve(board);
-        System.out.println(board);
-    }
+    
 
 
-    public boolean validator(int[][] board, int row, int col, int spot) {
+    public static boolean validator(int[][] board, int row, int col, int spot) {
         int boxrow = row-row%3;
         int boxcol = col-col%3;
         for(int i = 0; i<9; i++){
@@ -33,21 +21,25 @@ public class Solver {
         return true;
     }
 
-    public boolean solve(int[][] board){
+    public  static boolean solve(int[][] board){
         for(int i =0; i<board.length; i++){
-            for(int j = 0; j<board[0].length; j++){
+            for(int j = 0; j<board[i].length; j++){
                 if(board[i][j]== 0){
                     for(int k =1; k<=9; k++){
-                        if(validator(board, i, j, k))
+                        if(validator(board, i, j, k)){
                             board[i][j] = k;    
-                        if(solve(board))
-                            return true;
-                        else{
-                            board[i][j] = 0;
-                        }
-                    }
+                            if(solve(board))
+                                return true;
+                            else{
+                                board[i][j] = 0;
+                            }
 
-                    return false;
+
+                            
+                        }
+                        }
+
+                        return false;
                 }
             }
         }
